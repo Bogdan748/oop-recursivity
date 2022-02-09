20d5943fba103a1b92d234543f1b06102e4cdc17
Bogdan748 <andriesbogdan00@gmail.com>
2022-02-09 19:18:23 +0200

    implement factorial

---
+++ OopRecursivity/OopRecursivity/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace OopRecursivity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RecursivityHelper.PrintArray(new[] {1,2,3,4,5,6});
+
+            /*
+            int n = 5;
+            int rezult = RecursivityHelper.Factorial(n);
+
+            Console.WriteLine($"Fact de {n} is {rezult}");
+            */
+        }
+    }
+}
---
+++ OopRecursivity/OopRecursivity/RecursivityHelper.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopRecursivity
+{
+    public static class RecursivityHelper
+    {
+        public static int Factorial(int n)
+        {
+            if (n == 1 || n == 0) return 1;
+
+            return n * Factorial(n - 1);
+        }
+
+        public static void PrintArray(int[] array)
+        {
+            if (array is null || array.Length == 0)
+            {
+                return;
+            }
+            PrintArray(array, 0);
+        }
+
+
+        private static void PrintArray(int[] array, int i)
+        {
+            Console.Write(array[i]);
+            if (i < array.Length-1)
+            {
+                Console.Write(", ");
+                PrintArray(array, i + 1);
+            }
+        }
+
+
+    }
+}
